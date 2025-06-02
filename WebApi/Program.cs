1495a4f4c47731456d8b4cfc1a3c45f3422a614c
Joakim Erik Åkerman <joakim.akerman@hotmail.com>
2025-06-02 18:02:33 +0200

    testing cors

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -13,17 +13,27 @@ builder.Services.AddSingleton(x => new EmailClient(builder.Configuration["ACS:Co
 builder.Services.AddScoped<IVerificationService, VerificationService>();
 builder.Services.AddScoped<IBookingEmailService, BookingEmailService>();
 
+builder.Services.AddCors(o =>
+    o.AddPolicy("CorsPolicy", p =>
+        p.WithOrigins("https://lively-hill-0b76ba003.6.azurestaticapps.net")
+         .AllowAnyHeader()
+         .AllowAnyMethod()
+         .AllowCredentials()));
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+app.UseCors("CorsPolicy");
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run(); 
+app.Run();
